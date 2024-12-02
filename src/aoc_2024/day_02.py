c19e62a64e7dd71798246e7de182e80b0ea004bb
gregsym <gtsrex@gmail.com>
2024-12-02 21:34:14 +0200

    day 02 part 01

--- src/aoc_2024/day_02.py
+++ src/aoc_2024/day_02.py
@@ -1,8 +1,14 @@
 from aoc_2022.utils.day_handler import DayInterface
 from aoc_2022.utils.transforms import DataTransforms
+from aoc_2024.aoc_2024 import solve_day_02_pt_01
 import pytest
 
-test_input = """mjqjpqmgbljsphdztnvjfqwrcgsmlb"""
+test_input = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
 
 
 @pytest.fixture
@@ -13,12 +19,12 @@ def input():
 def solve_day(input: str) -> int:
     info = DataTransforms(input).lines  # manipulate input per usecase
     # solve
-    return 0
+    return solve_day_02_pt_01(input)
 
 
-def test_day_1_part_1(input: str) -> None:
+def test_day_2_part_1(input: str) -> None:
     # test solution to part 1
-    assert 7 == solve_day(input)
+    assert 2 == solve_day(input)
 
 
 # def test_day_1_part_2(input: str) -> None:
@@ -27,7 +33,7 @@ def test_day_1_part_1(input: str) -> None:
 
 
 if __name__ == "__main__":
-    real_input = DayInterface(1).get_day()
-    test_day_1_part_1(test_input)
+    real_input = DayInterface(2).get_day()
+    test_day_2_part_1(test_input)
     # test_day_1_part_2(test_input)
-    print(DayInterface(1).submit_day(solve_day(real_input)))
+    print(DayInterface(2).submit_day(solve_day(real_input)))
--- src/lib.rs
+++ src/lib.rs
@@ -22,10 +22,36 @@ fn solve_day_01_pt_02(input: String) -> PyResult<i32> {
     Ok(tally)
 }
 
+#[pyfunction]
+fn solve_day_02_pt_01(input: String) -> PyResult<i32> {
+    let reactor: Vec<Vec<i32>> = input
+        .split_terminator("\n")
+        .map(|row| {
+            row.split_terminator(" ").filter(|c| *c != "")
+                .map(|str_num| str_num.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect();
+    let mut tally = 0;
+    for row in reactor.iter() {
+        let zipped = row
+            .iter()
+            .zip(row.clone().split_off(1));
+        let purely_increasing = zipped.clone().all(|(first, second)| first < &second);
+        let purely_decreasing = zipped.clone().all(|(first, second)| first > &second);
+        let diff_under_eq_3 = zipped.clone().all(|(first, second)| (first - &second).abs() <= 3);
+        if (purely_increasing || purely_decreasing) && diff_under_eq_3 {
+            tally += 1;
+        }
+    }
+    Ok(tally)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn aoc_2024(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(solve_day_01_pt_02, m)?)?;
+    m.add_function(wrap_pyfunction!(solve_day_02_pt_01, m)?)?;
     Ok(())
 }
